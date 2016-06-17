945e38e9d7d37a401ff2a91c71cd950982668a12
Yukai Li <yukaili.geek@gmail.com>
2016-06-16 21:36:01 -0600

    Update script for use with Steamworks API 1.37

--- steam_user_stats_lite.rb
+++ steam_user_stats_lite.rb
@@ -1,29 +1,101 @@
-# cyanic's Quick and Easy Steamworks Achievements Integration for Ruby  
-# https://github.com/GMMan/RGSS_SteamUserStatsLite  
-# r3.2 03/12/16
+# cyanic's Quick and Easy Steamworks Achievements Integration for Ruby
+# https://github.com/GMMan/RGSS_SteamUserStatsLite
+# r4 06/16/16
 #
-# Drop steam_api.dll into the root of your project. Requires Steamworks SDK version >= 1.32.
+# Drop steam_api.dll into the root of your project. Requires Steamworks SDK version >= 1.37.
 #
 # "Miller complained about how hard achievements were to implement in C++, so this was born."
 #
 
 $imported ||= {}
-$imported['cyanic-SteamUserStatsLite'] = 3.2 # Slightly unorthodox, it's a version number.
+$imported['cyanic-SteamUserStatsLite'] = 4 # Slightly unorthodox, it's a version number.
+
+# A context class to get Steamworks pointers to interfaces.
+#
+# @author cyanic
+class SteamAPIContext
+  STEAMCLIENT_INTERFACE_VERSION = 'SteamClient017'
+  STEAMUSERSTATS_INTERFACE_VERSION = 'STEAMUSERSTATS_INTERFACE_VERSION011'
+  STEAMAPPS_INTERFACE_VERSION = 'STEAMAPPS_INTERFACE_VERSION008'
+
+  # Instantiates a new instance of `SteamAPIContext`.
+  def initialize
+    @initted = false
+    @h_steam_user = @@dll_SteamAPI_GetHSteamUser.call
+    if (@h_steam_pipe = @@dll_SteamAPI_GetHSteamPipe.call) != 0
+      return if (@steam_client = @@dll_SteamInternal_CreateInterface.call(STEAMCLIENT_INTERFACE_VERSION)) == 0
+      return if (@steam_user_stats = @@dll_SteamAPI_ISteamClient_GetISteamUserStats.call(@steam_client, @h_steam_user, @h_steam_pipe, STEAMUSERSTATS_INTERFACE_VERSION)) == 0
+      return if (@steam_apps = @@dll_SteamAPI_ISteamClient_GetISteamApps.call(@steam_client, @h_steam_user, @h_steam_pipe, STEAMAPPS_INTERFACE_VERSION)) == 0
+
+      @initted = true
+    end
+  end
+
+  # Checks if context is initialized.
+  #
+  # @return [true, false] Whether context is initialized.
+  def initted?
+    @initted
+  end
+
+  # Gets the ISteamClient pointer
+  #
+  # @return [Fixnum, nil] The ISteamClient pointer if context is initialized, otherwise `nil`.
+  def steam_client
+    @steam_client if initted?
+  end
+
+  # Gets the ISteamUserStats pointer
+  #
+  # @return [Fixnum, nil] The ISteamUserStats pointer if context is initialized, otherwise `nil`.
+  def steam_user_stats
+    @steam_user_stats if initted?
+  end
+
+  # Gets the ISteamApps pointer
+  #
+  # @return [Fixnum, nil] The ISteamUserStats pointer if context is initialized, otherwise `nil`.
+  def steam_apps
+    @steam_apps if initted?
+  end
+
+  private
+  def self.is_64bit?
+    # Probably very bad detection of whether current runtime is 64-bit
+    (/x64/ =~ RUBY_PLATFORM) != nil
+  end
+
+  def self.steam_dll_name
+    @@dll_name ||= self.is_64bit? ? 'steam_api64' : 'steam_api'
+  end
+
+  @@dll_SteamAPI_GetHSteamUser = Win32API.new(self.steam_dll_name, 'SteamAPI_GetHSteamUser', '', 'I')
+  @@dll_SteamAPI_GetHSteamPipe = Win32API.new(self.steam_dll_name, 'SteamAPI_GetHSteamPipe', '', 'I')
+  @@dll_SteamInternal_CreateInterface = Win32API.new(self.steam_dll_name, 'SteamInternal_CreateInterface', 'P', 'I')
+  @@dll_SteamAPI_ISteamClient_GetISteamUserStats = Win32API.new(self.steam_dll_name, 'SteamAPI_ISteamClient_GetISteamUserStats', 'IIIP', 'I')
+  @@dll_SteamAPI_ISteamClient_GetISteamApps = Win32API.new(self.steam_dll_name, 'SteamAPI_ISteamClient_GetISteamApps', 'IIIP', 'I')
+
+end
 
 # A simple class for Steamworks UserStats integration.
 #
 # @author cyanic
 class SteamUserStatsLite
-  
+
   # Instantiates a new instance of `SteamUserStatsLite`.
   def initialize
-    @initted = @@dll_SteamAPI_Init.call % 256 != 0
-    if @initted
-      @i_apps = @@dll_SteamApps.call
-      @i_user_stats = @@dll_SteamUserStats.call
+    @initted = false
+    api_initted = @@dll_SteamAPI_Init.call % 256 != 0
+    if api_initted
+      @context = SteamAPIContext.new
+      if @context.initted?
+        @i_apps = @context.steam_apps
+        @i_user_stats = @context.steam_user_stats
+        @initted = true
+      end
     end
   end
-  
+
   # Shuts down Steamworks.
   #
   # @return [void]
@@ -33,31 +105,31 @@ class SteamUserStatsLite
       @i_user_stats = nil
       @@dll_SteamAPI_Shutdown.call
       @initted = false
-    end    
+    end
   end
-  
+
   # Checks if Steamworks is initialized.
   #
   # @return [true, false] Whether Steamworks is initialized.
   def initted?
     @initted
   end
-  
+
   # Restarts the app if Steamworks is not availble.
   #
   # @param app_id [Integer] The app ID to relaunch as.
-  # @return [true, false] `true` if current instance should exit, `false` if not. 
+  # @return [true, false] `true` if current instance should exit, `false` if not.
   def self.restart_app_if_necessary(app_id)
     @@dll_SteamAPI_RestartAppIfNecessary.call(app_id) % 256 != 0
   end
-  
+
   # Runs Steam callbacks.
   #
   # @return [void]
   def update
     @@dll_SteamAPI_RunCallbacks.call if initted?
   end
-  
+
   # Checks if current app is owned.
   #
   # @return [true, false, nil] Whether the current user has a license for the current app. `nil` is returned if ownership status can't be retrieved.
@@ -68,7 +140,7 @@ class SteamUserStatsLite
       nil
     end
   end
-  
+
   # Checks if a DLC is installed.
   #
   # @param app_id [Integer] The app ID of the DLC to check.
@@ -80,7 +152,7 @@ class SteamUserStatsLite
       nil
     end
   end
-  
+
   # Pulls current user's stats from Steam.
   #
   # @return [true, false] Whether the stats have been successfully pulled.
@@ -91,7 +163,7 @@ class SteamUserStatsLite
       false
     end
   end
-  
+
   # Gets the value of an INT stat.
   #
   # @param name [String] The name of the stat.
@@ -105,7 +177,7 @@ class SteamUserStatsLite
       nil
     end
   end
-  
+
   # Gets the value of an FLOAT stat.
   #
   # @param name [String] The name of the stat.
@@ -119,7 +191,7 @@ class SteamUserStatsLite
       nil
     end
   end
-  
+
   # Sets the value of a stat.
   #
   # @param name [String] The name of the stat.
@@ -136,12 +208,12 @@ class SteamUserStatsLite
       else
         ok = @@dll_SteamAPI_ISteamUserStats_SetStat.call(@i_user_stats, name, val.to_i) % 256 != 0
       end
-      @@dll_SteamAPI_ISteamUserStats_StoreStats.call(@i_user_stats) % 256 != 0 && ok      
+      @@dll_SteamAPI_ISteamUserStats_StoreStats.call(@i_user_stats) % 256 != 0 && ok
     else
       false
     end
   end
-  
+
   # Updates an AVGRATE stat.
   #
   # @param name [String] The name of the stat.
@@ -152,12 +224,12 @@ class SteamUserStatsLite
     if initted?
       packed = self.class.pack_double session_length
       ok = @@dll_SteamAPI_ISteamUserStats_UpdateAvgRateStat.call(@i_user_stats, name, self.class.pack_float(count_this_session.to_f), packed[0], packed[1]) % 256 != 0
-      @@dll_SteamAPI_ISteamUserStats_StoreStats.call(@i_user_stats) % 256 != 0 && ok      
+      @@dll_SteamAPI_ISteamUserStats_StoreStats.call(@i_user_stats) % 256 != 0 && ok
     else
       false
     end
   end
-  
+
   # Gets an achievement's state.
   #
   # @param name [String] The name of the achievement.
@@ -171,7 +243,7 @@ class SteamUserStatsLite
       nil
     end
   end
-  
+
   # Sets an achievement as unlocked.
   #
   # @param name [String] The name of the achievement.
@@ -188,7 +260,7 @@ class SteamUserStatsLite
       false
     end
   end
-  
+
   # Sets an achievement as locked.
   #
   # @param name [String] The name of the achievement.
@@ -199,13 +271,13 @@ class SteamUserStatsLite
       @@dll_SteamAPI_ISteamUserStats_StoreStats.call(@i_user_stats) % 256 != 0 && ok
     else
       false
-    end    
+    end
   end
-  
+
   # Gets an achievement's state and unlock time.
   #
   # @param name [String] The name of the achievement.
-  # @return [<Object, Time>] The achievement's state (`true` or `false`) and the time it was unlocked. 
+  # @return [<Object, Time>] The achievement's state (`true` or `false`) and the time it was unlocked.
   def get_achievement_and_unlock_time(name)
     if initted?
       achieved = ' '
@@ -216,7 +288,7 @@ class SteamUserStatsLite
       nil
     end
   end
-  
+
   # Gets the value of an achievement's display attribute.
   #
   # @param name [String] The name of the achievement.
@@ -229,7 +301,7 @@ class SteamUserStatsLite
       nil
     end
   end
-  
+
   # Gets the number of achievements.
   #
   # @return [Integer, nil] The number of achievements, or `nil` if the number cannot be retrieved.
@@ -240,7 +312,7 @@ class SteamUserStatsLite
       nil
     end
   end
-  
+
   # Gets the name of an achievement by its index.
   #
   # @param achievement [Integer] The index of the achievement.
@@ -252,7 +324,7 @@ class SteamUserStatsLite
       nil
     end
   end
-  
+
   # Resets all stats.
   #
   # @param achievements_too [true, false] Whether to reset achievements as well.
@@ -265,30 +337,29 @@ class SteamUserStatsLite
       false
     end
   end
-  
+
   # Gets the global instance of SteamUserStatsLite.
   #
   # @return [SteamUserStatsLite] The global instance of the class.
   def self.instance
     @@instance
   end
-  
+
   private
   def self.is_64bit?
     # Probably very bad detection of whether current runtime is 64-bit
     (/x64/ =~ RUBY_PLATFORM) != nil
   end
-  
+
   def self.steam_dll_name
     @@dll_name ||= self.is_64bit? ? 'steam_api64' : 'steam_api'
   end
-  
+
   # Function imports
   @@dll_SteamAPI_RestartAppIfNecessary = Win32API.new(self.steam_dll_name, 'SteamAPI_RestartAppIfNecessary', 'I', 'I')
   @@dll_SteamAPI_Init = Win32API.new(self.steam_dll_name, 'SteamAPI_Init', '', 'I')
   @@dll_SteamAPI_Shutdown = Win32API.new(self.steam_dll_name, 'SteamAPI_Shutdown', '', 'V')
   @@dll_SteamAPI_RunCallbacks = Win32API.new(self.steam_dll_name, 'SteamAPI_RunCallbacks', '', 'V')
-  @@dll_SteamUserStats = Win32API.new(self.steam_dll_name, 'SteamUserStats', '', 'N')
   @@dll_SteamAPI_ISteamUserStats_RequestCurrentStats = Win32API.new(self.steam_dll_name, 'SteamAPI_ISteamUserStats_RequestCurrentStats', 'P', 'I')
   @@dll_SteamAPI_ISteamUserStats_GetStat = Win32API.new(self.steam_dll_name, 'SteamAPI_ISteamUserStats_GetStat', 'PPP', 'I')
   @@dll_SteamAPI_ISteamUserStats_GetStat0 = Win32API.new(self.steam_dll_name, 'SteamAPI_ISteamUserStats_GetStat0', 'PPP', 'I')
@@ -304,18 +375,17 @@ class SteamUserStatsLite
   @@dll_SteamAPI_ISteamUserStats_GetAchievementName = Win32API.new(self.steam_dll_name, 'SteamAPI_ISteamUserStats_GetAchievementName', 'PI', 'P')
   @@dll_SteamAPI_ISteamUserStats_StoreStats = Win32API.new(self.steam_dll_name, 'SteamAPI_ISteamUserStats_StoreStats', 'P', 'I')
   @@dll_SteamAPI_ISteamUserStats_ResetAllStats = Win32API.new(self.steam_dll_name, 'SteamAPI_ISteamUserStats_ResetAllStats', 'PI', 'I')
-  @@dll_SteamApps = Win32API.new(self.steam_dll_name, 'SteamApps', '', 'N')
   @@dll_SteamAPI_ISteamApps_BIsSubscribed = Win32API.new(self.steam_dll_name, 'SteamAPI_ISteamApps_BIsSubscribed', 'P', 'I')
   @@dll_SteamAPI_ISteamApps_BIsDlcInstalled = Win32API.new(self.steam_dll_name, 'SteamAPI_ISteamApps_BIsDlcInstalled', 'PI', 'I')
-  
+
   @@instance = self.new
-  
+
   def self.pack_float(val)
     # Packs number to a string, then unpack to an int
     inter = [val].pack 'e'
     inter.unpack('I')[0]
   end
-  
+
   def self.pack_double(val)
     # Packs number to a string, then unpack to an array of two ints
     inter = [val].pack 'd'
